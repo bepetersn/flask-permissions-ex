f85782c958a7055c01a856de12e69789680f954e
Brian Peterson <bepetersn@gmail.com>
2016-02-12 12:34:42 -0700

    Add in permissions

--- ex/__init__.py
+++ ex/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_login import LoginManager, current_user
+from flask_permissions.core import Permissions
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+perms = Permissions(app, db, current_user)
 
 app.config['SECRET_KEY'] = 'FAKE'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- ex/database.py
+++ ex/database.py
@@ -1,21 +1,27 @@
-
+#from flask_login import UserMixin as UserLoginMixin
+from flask_permissions.models import UserMixin as UserPermissionMixin
 from werkzeug.security import generate_password_hash
 from sqlalchemy import Column, Unicode, Integer
 from . import db
 
 
-class User(db.Model):
+class User(UserPermissionMixin):
 
     id = Column(Integer, primary_key=True)
     email = Column(Unicode(255))
     password_hash = Column(Unicode(71))
+
+    # Identify the class to differentiate between sub-types of the UserMixin.
+    __mapper_args__ = {
+        'polymorphic_identity': 'user' # This can be any unique value except 'usermixin'.
+    }
 
     __tablename__ = 'users'
 
     def __init__(self, email, password, roles=None):
         self.email = email
         self.set_password(password)
-        # UserPermissionMixin.__init__(self, roles)
+        UserPermissionMixin.__init__(self, roles)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
