c67cba426efdde820e58226d58976014e977b583
Brian Peterson <bepetersn@gmail.com>
2016-02-20 02:16:04 -0700

    Fix mysql_connnection helper mistake

--- ex/db_utils.py
+++ ex/db_utils.py
@@ -5,6 +5,6 @@ from .database import connection_url
 def mysql_connection():
     return MySQLdb.connect(
         user=connection_url.username,
-        passwd=connection_url.engine.url.password
+        passwd=connection_url.password
     )
 
