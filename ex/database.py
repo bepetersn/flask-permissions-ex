cdf8405e7e6629ac3aaf72aaea98f8b2d0d5317f
Brian Peterson <bepetersn@gmail.com>
2016-07-30 13:47:30 -0600

    Switch over the User model code to @leoGalani

--- ex/database.py
+++ ex/database.py
@@ -1,7 +1,6 @@
 #from flask_login import UserMixin as UserLoginMixin
+import bcrypt
 from flask_permissions.models import UserMixin
-from werkzeug.security import generate_password_hash
-from sqlalchemy import Column, Unicode, Integer
 from . import db
 
 
@@ -9,17 +8,16 @@ connection_url = db.engine.url
 
 
 class User(UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
 
-    id = Column(Integer, primary_key=True)
-    email = Column(Unicode(255))
-    password_hash = Column(Unicode(71))
-
-    __tablename__ = 'users'
-
-    def __init__(self, email, password, roles=None):
-        self.email = email
-        self.set_password(password)
+    def __init__(self, name, email, password, roles=None):
+        """Setting params to the object."""
+        import pdb; pdb.set_trace()
+        self.name = name
+        self.email = email.lower()
+        self.password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt())
         UserMixin.__init__(self, roles)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
